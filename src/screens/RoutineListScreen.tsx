c481d210c8c4701c5de87ff6e5def1e2bff75a5a
luisAristeguieta <luisjimenezgx@gmail.com>
2026-09-22 23:31:16 -0400

    feat(screens): implement dynamic routine list with delete action

--- src/screens/RoutineListScreen.tsx
+++ src/screens/RoutineListScreen.tsx
@@ -1,8 +1,25 @@
-import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image, Alert,FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
+import { useRoutineContext} from '../context/RoutineContext';
+
 
 export default function RoutineListScreen({ navigation }: any) {
+
+  const { routines, deleteRoutine } = useRoutineContext();
+
+  const handleDelete = (id: string, name: string) => {
+    Alert.alert(
+      'Eliminar Rutina',
+      `¿Estás seguro de que deseas eliminar "${name}"?`,
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        { text: 'Eliminar', style: 'destructive', onPress: () => deleteRoutine(id) },
+      ]
+    );
+  };
+
+
   return (
     <SafeAreaView style={styles.safeArea}>
       {/* SECCIÓN SUPERIOR: Logo Oficial (Flexbox) */}
@@ -18,21 +35,54 @@ export default function RoutineListScreen({ navigation }: any) {
       <View style={styles.bottomSection}>
         <View style={styles.cardHeader}>
           <Ionicons name="flame" size={24} color="#FF6B00" />
-          <Text style={styles.cardTitle}>Rutina Destacada</Text>
+          <Text style={styles.cardTitle}>Rutinas Destacada</Text>
         </View>
 
-        <Text style={styles.cardSubtitle}>
-          Enfoque de fuerza e hipertrofia para pectoral mayor y tríceps.
-        </Text>
-
+        <View >
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('ChestDetail')}
-          activeOpacity={0.85}
-        >
-          <Text style={styles.buttonText}>Ver Rutina de Pecho</Text>
-          <Ionicons name="arrow-forward" size={20} color="#FFFFFF" />
+          onPress={() => navigation.navigate('AddRoutine')}>
+          <Ionicons name="add" size={22} color="#FFFFFF" />
+          <Text >Agregar Rutina</Text>
         </TouchableOpacity>
+      </View>
+
+      {/* Listado de rutinas */}
+
+      <FlatList
+        data={routines}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View >
+            <View >
+              <Text >{item.name}</Text>
+              <Text >{item.muscleGroup}</Text>
+              <Text >{item.duration} min </Text>
+            </View>
+            <View >
+              {/* Ver Rutina */}
+              <TouchableOpacity
+                onPress={() => navigation.navigate('ChestDetailScreen', { routineId: item.id })}
+              >
+                <Ionicons name="eye-outline" size={20} color="#3226D7" />
+              </TouchableOpacity>
+
+              {/* Editar Rutina*/}
+              <TouchableOpacity
+                onPress={() => navigation.navigate('AddRoutine', { routineId: item.id })}
+              >
+                <Ionicons name="pencil-outline" size={20} color="#D75826" />
+              </TouchableOpacity>
+
+              {/* Eliminar directo con confirmación */}
+              <TouchableOpacity
+                onPress={() => handleDelete(item.id, item.name)}
+              >
+                <Ionicons name="trash-outline" size={20} color="#F10303" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
       </View>
     </SafeAreaView>
   );
